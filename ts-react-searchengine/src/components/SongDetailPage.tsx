4b4dc912e51628122b92c69b464428d337798b5d
iCarambaaa <dkpress1916@zoho.eu>
2021-12-01 17:19:44 +0100

    Update SongDetailPage.tsx

--- ts-react-searchengine/src/components/SongDetailPage.tsx
+++ ts-react-searchengine/src/components/SongDetailPage.tsx
@@ -1,13 +1,36 @@
+import {useParams} from 'react-router'
+import { useEffect, useState } from 'react'
+import {Song} from "../interfaces/songs" 
 
-
-// interface SongDetailPageProps {
-//     songID: number
-// }
 
 const SongDetailPage = () => {
 
+    const [track, setTrack] = useState<Song>()
+
+const {id} = useParams()
+//console.log(id)
+const endpoint = `https://striveschool-api.herokuapp.com/api/deezer/track/${id}`
+// console.log(endpoint) 
+const getDetails = async() => {
+    try {
+        let response = await fetch(endpoint)
+        if (response.ok) {
+            let data: Song = await response.json()
+            console.log(data) 
+            setTrack(data)
+        } else {
+        console.log("ERR")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+useEffect(() => {
+    getDetails()
+  }, [])
+
 return (
-    <div>hello</div>
+    <div>{track?.title}</div>
 )
 
 }
