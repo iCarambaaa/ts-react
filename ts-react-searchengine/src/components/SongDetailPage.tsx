2fdd4b4f8ea4b68df63aeccaf108de907939e825
iCarambaaa <dkpress1916@zoho.eu>
2021-12-01 17:59:29 +0100

    Update SongDetailPage.tsx

--- ts-react-searchengine/src/components/SongDetailPage.tsx
+++ ts-react-searchengine/src/components/SongDetailPage.tsx
@@ -1,5 +1,5 @@
 import {useParams} from 'react-router'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import {Song} from "../interfaces/songs" 
 
 
@@ -11,26 +11,32 @@ const {id} = useParams()
 //console.log(id)
 const endpoint = `https://striveschool-api.herokuapp.com/api/deezer/track/${id}`
 // console.log(endpoint) 
-const getDetails = async() => {
-    try {
-        let response = await fetch(endpoint)
-        if (response.ok) {
-            let data: Song = await response.json()
-            console.log(data) 
-            setTrack(data)
-        } else {
-        console.log("ERR")
+const getDetails = useCallback(
+    async() => {
+        try {
+            let response = await fetch(endpoint)
+            if (response.ok) {
+                let data: Song = await response.json()
+                console.log(data) 
+                setTrack(data)
+            } else {
+            console.log("ERR")
+            }
+        } catch (error) {
+            console.log(error)
         }
-    } catch (error) {
-        console.log(error)
-    }
-}
+    
+}, [endpoint])
 useEffect(() => {
     getDetails()
-  }, [])
+  }, [getDetails])
 
 return (
-    <div>{track?.title}</div>
+    <div>
+        <h2>{track?.title}</h2>
+        <img src={track?.album.cover_big} alt={track?.album.title}></img>
+        </div>
+
 )
 
 }
